6e78f773b8bb7006c9633ccf7d43f176119feeaf
Arina Goloubitskaya <arishago@google.com>
2020-08-03 15:12:23 +0000

    Extracting trivial information (name, zone, machne_type) about changed resources

--- parsing-json.go
+++ parsing-json.go
@@ -6,13 +6,38 @@ import (
     "encoding/json"
     "io/ioutil"
 )
+type ComputeInstanceInfo struct {
+    Name string
+    MachineType string
+    Zone string
 
+    IsNil bool
+}
+
+type ComputeInstance struct {
+    BeforeState ComputeInstanceInfo
+    AfterState ComputeInstanceInfo
+}
+
+//extract the info about compute instance from go interface
+//we call it for before and after json for every change in resource_changes
+func ExtractInstanceInfo (change interface{}) ComputeInstanceInfo {
+    var computeInstance ComputeInstanceInfoa
+    if change != nil {
+        change := change.(map[string]interface{})
+        computeInstance.IsNil = false
+        computeInstance.Name = change["name"].(string)
+        computeInstance.MachianeType = change["machine_type"].(string)
+        computeInstance.Zone = change["zone"].(string)
+    } else {
+        computeInstance.IsNil = true
+    }
+    return computeInstance
+}
 func main() {
-	fmt.Println("It works! =)")
-	fmt.Println("JSON file name:", os.Args[1])
-
+ 	fmt.Println("JSON file name:", os.Args[1])
 	jsonFileName := os.Args[1]
-	jsonFile, err := os.Open(jsonFileName)
+    jsonFile, err := os.Open(jsonFileName)
 
     if err != nil {
 	// Handle error.
@@ -24,18 +49,35 @@ func main() {
     byteValue, _ := ioutil.ReadAll(jsonFile)
     var result map[string]interface{}
     json.Unmarshal([]byte(byteValue), &result)
+
+    //list of the resources changes
     resourceChanges := result["resource_changes"].([]interface{})
 
+    //list of changed compute instances
+    var ChangedComputeInstances []ComputeInstance
+
+
+    var computeInstance ComputeInstance
+    // var beforeInfo ComputeInstanceInfo
+    // var afterInfo ComputeInstanceInfo
     for _, c := range resourceChanges {
+        //cast c to map struct
         cmap := c.(map[string]interface{})
         change := cmap["change"].(map[string]interface{})
-        before := change["before"]
-        if before != nil {
-            before := before.(map[string]interface{})
-            fmt.Println(before)
-        } else {
-            fmt.Println("before = nil")
-        }
-    }
     
+        // beforeInfo = ExtractInstanceInfo(change["before"])
+        // afterInfo = ExtractInstanceInfo(change["after"])
+        // fmt.Println(beforeInfo.Name)
+        // fmt.Println(beforeInfo.MachineType)
+        // fmt.Println(beforeInfo.Zone)
+        // fmt.Println(afterInfo.Name)
+
+        computeInstance.BeforeState = ExtractInstanceInfo(change["before"])
+        computeInstance.AfterState = ExtractInstanceInfo(change["after"])
+        fmt.Println(computeInstance.BeforeState.Name)
+        fmt.Println(computeInstance.BeforeState.MachineType)
+        fmt.Println(computeInstance.AfterState.Name)
+        ChangedComputeInstances = append(ChangedComputeInstances, computeInstance)
+    }
+    fmt.Println(ChangedComputeInstances)
 }
