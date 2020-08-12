cfbd2897c7b552cf23e82dc4b66aee2a00e30254
sweetd0ve <68424950+sweetd0ve@users.noreply.github.com>
2020-08-12 11:25:32 +0300

    Update decode_json.go

--- jsdecode/decode_json.go
+++ jsdecode/decode_json.go
@@ -1,9 +1,7 @@
 package jsdecode
 
 import (
-	//"encoding/json"
-	"errors"
-	"fmt"
+	"encoding/json"
 	resources "github.com/googleinterns/terraform-cost-estimation/resources"
 	tfjson "github.com/hashicorp/terraform-json"
 	"io/ioutil"
@@ -14,9 +12,7 @@ import (
 // by this package, we consider ComputeInstances only for now.
 const ComputeIstanceType = "google_compute_instance"
 
-// PlanFormatVersion is the supported version of the JSON plan format.
-const PlanFormatVersion = "0.1"
-
+// TODO maybe add mem/core info
 // ResourceInfo conatains the information about Resource, this struct is
 // used to cast interfaces with before/after states of the certain Resource.
 type ResourceInfo struct {
@@ -45,60 +41,30 @@ func ExtractPlanStruct(filePath string) (*tfjson.Plan, error) {
 
 	byteFile, _ := ioutil.ReadAll(f)
 
-	var plan *tfjson.Plan
-	if plan, err := UnmarshalJSON(byteFile); err != nil {
+	var plan tfjson.Plan
+	if err = plan.UnmarshalJSON(byteFile); err != nil {
 		return nil, err
 	}
-	return plan, nil
+	return &plan, nil
 }
 
-// TODO!
-// complete Unmarshalling function with ResearchInfo in plan.ResourceChanges
-// for every ResourceChange in Change.before/after  instead of interface{}
-func UnmarshalJSON(b []byte) (*tfjson.Plan, error) {
-// 	var p tfjson.Plan
-
-// 	type rawPlan tfjson.Plan
-// 	var plan rawPlan
-// 	if err := json.Unmarshal(b, &plan); err != nil {
-// 		return nil, err
-// 	}
-
-// 	// case p != nill ...
-// 	return &p, Validate(&p)
-	return nil, nil
-}
-
-// Validate checks to ensure that the plan is present, and the
-// version matches PlanFormatVersion.
-func Validate(plan *tfjson.Plan) error {
-	if plan == nil {
-		return errors.New("plan is nil")
-	}
-
-	if plan.FormatVersion == "" {
-		return errors.New("unexpected plan input, it has to contain a format version field")
-	}
-
-	if PlanFormatVersion != plan.FormatVersion {
-		return fmt.Errorf("plan format version not supported: expected %q, got %q", PlanFormatVersion, plan.FormatVersion)
-	}
-
-	return nil
-}
-
-// ExtractResource extructs ComputeInstance from the struct that
-// contains information about resource.
-func ExtractResource(resource *ResourceInfo) *resources.ComputeInstance {
+// ExtractResource extructs ComputeInstance from the interface
+// containing information about resource.
+func toComputeInstance(resource interface{}) *resources.ComputeInstance {
 	if resource == nil {
 		return nil
 	}
+
+	jsonString, _ := json.Marshal(resource)
+	var r ResourceInfo
+	json.Unmarshal(jsonString, &r)
 	return &resources.ComputeInstance{
-		ID:          resource.ID,
-		Name:        resource.Name,
-		MachineType: resource.MachineType,
-		//TODO consider case where zone empty if it is possible in valid json
-		Region:      resource.Zone[:len(resource.Zone)-2],
+        // TODO check if unmarshaled resource is valid and zone>2 chars
+        // add core/memory for some types of ComputeInstances
+		ID:          r.ID,
+		Name:        r.Name,
+		MachineType: r.MachineType,
+		Region:      r.Zone[:len(r.Zone)-2],
 	}
 }
 
@@ -106,8 +72,8 @@ func ExtractResource(resource *ResourceInfo) *resources.ComputeInstance {
 // certain resource of ComputeInstance type.
 func GetChange(change *tfjson.Change) *ResourceStates {
 	resource := &ResourceStates{
-		Before: ExtractResource(change.Before),
-		After:  ExtractResource(change.After),
+		Before: toComputeInstance(change.Before),
+		After:  toComputeInstance(change.After),
 	}
 	if resource.Before == nil && resource.After == nil {
 		return nil
