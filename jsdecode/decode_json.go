d87df7cb78c041789c6c6e5f0a5ceae8747d9e06
sweetd0ve <68424950+sweetd0ve@users.noreply.github.com>
2020-08-24 13:53:35 +0000

    Update decode_json.go

--- jsdecode/decode_json.go
+++ jsdecode/decode_json.go
@@ -2,6 +2,7 @@ package jsdecode
 
 import (
 	"encoding/json"
+	"fmt"
 	resources "github.com/googleinterns/terraform-cost-estimation/resources"
 	tfjson "github.com/hashicorp/terraform-json"
 	"io"
@@ -12,39 +13,51 @@ import (
 // by this package, we consider ComputeInstances only for now.
 const ComputeInstanceType = "google_compute_instance"
 
-// TODO maybe add mem/core info
+// Possible Actions in ComputeInstanceState.
+const (
+	ActionNoop    string = "no-op"
+	ActionCreate  string = "create"
+	ActionDelete  string = "delete"
+	ActionUpdate  string = "update"
+	ActionReplace string = "replace"
+)
+
 // ResourceInfo contains the information about Resource, this struct is
 // used to cast interfaces with before/after states of the certain Resource.
 type ResourceInfo struct {
-	ID          string `json:"id,omitempty"`
-	InstanceID  string `json:"instance_id"`
-	Name        string `json:"name,omitempty"`
-	MachineType string `json:"machine_type,omitempty"`
-	Zone        string `json:"zone,omitempty"`
+	ID          string      `json:"id,omitempty"`
+	InstanceID  string      `json:"instance_id"`
+	Name        string      `json:"name,omitempty"`
+	MachineType string      `json:"machine_type,omitempty"`
+	Zone        string      `json:"zone,omitempty"`
+	Scheduling  []UsageType `json:"scheduling,omitempty"`
 }
 
-// ResourceStates contains information about the states of resource
-// of type ComputeInstance in plan configuration.
-type ResourceStates struct {
-	Before *resources.ComputeInstance
-	After  *resources.ComputeInstance
+// UsageType contains the information whether the certain istance
+// is preemptible or not.
+type UsageType struct {
+	IsPreemptible bool `json:"preemptible,omitempty"`
 }
 
 // ExtractPlanStruct extracts tfjson.Plan struct from file in provided path
 // and returns the pointer on it if it is possible, otherwise returns error.
 func ExtractPlanStruct(reader io.Reader) (*tfjson.Plan, error) {
-	bytes, _ := ioutil.ReadAll(reader)
+	bytes, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
 
 	var plan tfjson.Plan
-	if err := plan.UnmarshalJSON(bytes); err != nil {
+	err = plan.UnmarshalJSON(bytes)
+	if err != nil {
 		return nil, err
 	}
 	return &plan, nil
 }
 
-// ToComputeInstance extracts ComputeInstance from the interface that
-// contains information about resource.
-func ToComputeInstance(resource interface{}) (*resources.ComputeInstance, error) {
+// toComputeInstance extracts ComputeInstance from the interface that
+// contains information about the resource.
+func toComputeInstance(resource interface{}) (*resources.ComputeInstance, error) {
 	if resource == nil {
 		return nil, nil
 	}
@@ -69,29 +82,54 @@ func ToComputeInstance(resource interface{}) (*resources.ComputeInstance, error)
 		region = r.Zone
 	}
 
+	usageType := "OnDemand"
+ 	if len(r.Scheduling) >= 1 && r.Scheduling[0].IsPreemptible {
+		usageType = "Preemptible"
+	}
+	//TODO add resourceGroup in mem/core info
 	return &resources.ComputeInstance{
-		// TODO add core/memory for some types of ComputeInstances
 		ID:          r.ID,
 		Name:        r.Name,
 		MachineType: r.MachineType,
 		Region:      region,
+		UsageType:   usageType,
 	}, nil
 }
 
 // GetChange returns the pointer to the struct with states of the
 // certain resource of ComputeInstance type.
-func GetChange(change *tfjson.Change) (*ResourceStates, error) {
-	before, err := ToComputeInstance(change.Before)
+func GetChange(change *tfjson.Change) (*resources.ComputeInstanceState, error) {
+	before, err := toComputeInstance(change.Before)
 	if err != nil {
 		return nil, err
 	}
-	after, err := ToComputeInstance(change.After)
+	after, err := toComputeInstance(change.After)
 	if err != nil {
 		return nil, err
 	}
-	resource := &ResourceStates{
+
+	//TODO consider action read
+	actions := change.Actions
+	var action string
+	switch true {
+	case actions.NoOp():
+		action = ActionNoop
+	case actions.Create():
+		action = ActionCreate
+	case actions.Delete():
+		action = ActionDelete
+	case actions.Update():
+		action = ActionUpdate
+	case actions.Replace():
+		action = ActionReplace
+	default:
+		return nil, fmt.Errorf("Wrong action provided.")
+	}
+
+	resource := &resources.ComputeInstanceState{
 		Before: before,
 		After:  after,
+		Action: action,
 	}
 	if resource.Before == nil && resource.After == nil {
 		return nil, nil
@@ -101,8 +139,8 @@ func GetChange(change *tfjson.Change) (*ResourceStates, error) {
 
 // GetResources extracts all resources of ComputeInstance type and their
 // before and after states from plan file.
-func GetResources(plan *tfjson.Plan) []*ResourceStates {
-	var resources []*ResourceStates
+func GetResources(plan *tfjson.Plan) []*resources.ComputeInstanceState {
+	var resources []*resources.ComputeInstanceState
 	for _, resourceChange := range plan.ResourceChanges {
 		if ComputeInstanceType == resourceChange.Type {
 			if resource, err := GetChange(resourceChange.Change); resource != nil && err == nil {
