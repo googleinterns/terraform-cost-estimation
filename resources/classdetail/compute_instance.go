2b5bc8ef9d039d3417b2fd7cedbf37ad2427bf1b
StefaniaDamian <stefaniadamian@google.com>
2020-08-26 09:16:51 +0000

    updated file reading

--- resources/classdetail/compute_instance.go
+++ resources/classdetail/compute_instance.go
@@ -4,7 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"os"
+	"path/filepath"
+	"runtime"
 )
 
 type computeInstance struct {
@@ -12,21 +13,32 @@ type computeInstance struct {
 	MemoryGB   float64
 }
 
-var machineTypes = getMachineTypes()
+var machineTypes map[string]computeInstance
 
-func getMachineTypes() map[string]computeInstance {
-	f, _ := os.Open("machine_types.json")
-	defer f.Close()
+func getMachineTypes() (map[string]computeInstance, error) {
+	_, callerFile, _, _ := runtime.Caller(0)
+	inputPath := filepath.Dir(callerFile) + "/machine_types.json"
 
-	data, _ := ioutil.ReadAll(f)
+	data, err := ioutil.ReadFile(inputPath)
+	if err != nil {
+		return nil, err
+	}
 
 	var jsonMap map[string]computeInstance
 	json.Unmarshal(data, &jsonMap)
-	return jsonMap
+	return jsonMap, nil
 }
 
 // GetMachineDetails returns the number of cores and GBs of memory for a specific machine type.
 func GetMachineDetails(machineType string) (coreNum int, memGB float64, err error) {
+	if machineTypes == nil {
+		machineTypes, err = getMachineTypes()
+		if err != nil {
+			return 0, 0, err
+
+		}
+	}
+
 	d, ok := machineTypes[machineType]
 	if !ok {
 		return 0, 0, fmt.Errorf("machine type not supported")
