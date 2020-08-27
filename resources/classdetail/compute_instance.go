0a429706314402b897791048099c76c5d16f026c
StefaniaDamian <stefaniadamian@google.com>
2020-08-27 12:35:24 +0000

    added custom machine building

--- resources/classdetail/compute_instance.go
+++ resources/classdetail/compute_instance.go
@@ -6,6 +6,10 @@ import (
 	"io/ioutil"
 	"path/filepath"
 	"runtime"
+	"strconv"
+	"strings"
+
+	memconv "github.com/googleinterns/terraform-cost-estimation/memconverter"
 )
 
 type computeInstance struct {
@@ -29,6 +33,42 @@ func getMachineTypes() (map[string]computeInstance, error) {
 	return jsonMap, nil
 }
 
+func getCustomMachineDetails(machineType string) (coreNum int, memGiB float64, err error) {
+	if strings.HasSuffix(machineType, "-ext") {
+		i := strings.LastIndex(machineType, "-")
+		machineType = machineType[:i]
+	}
+
+	i := strings.LastIndex(machineType, "-")
+	if i < 0 {
+		return 0, 0, fmt.Errorf("invalid custom machine type format")
+	}
+	memStr := machineType[i+1:]
+	machineType = machineType[:i]
+
+	i = strings.LastIndex(machineType, "-")
+	if i < 0 {
+		return 0, 0, fmt.Errorf("invalid custom machine type format")
+	}
+	coresStr := machineType[i+1:]
+
+	coreNum, err = strconv.Atoi(coresStr)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	memMiB, err := strconv.Atoi(memStr)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	memGiB, err = memconv.Convert("mib", float64(memMiB), "gib")
+	if err != nil {
+		return 0, 0, err
+	}
+	return
+}
+
 // GetMachineDetails returns the number of cores and GBs of memory for a specific machine type.
 func GetMachineDetails(machineType string) (coreNum int, memGB float64, err error) {
 	if machineTypes == nil {
@@ -37,6 +77,10 @@ func GetMachineDetails(machineType string) (coreNum int, memGB float64, err erro
 			return 0, 0, err
 
 		}
+	}
+
+	if strings.Contains(machineType, "custom") {
+		return getCustomMachineDetails(machineType)
 	}
 
 	d, ok := machineTypes[machineType]
