c64aa9893563c5cffa97d4f1a529689a8887c661
StefaniaDamian <68544330+StefaniaDamian@users.noreply.github.com>
2020-09-02 12:55:32 +0000

    Io (#54)
    * Update resource.go
    
    * added support for multiple inputs/outputs
    
    * fixed test

--- main.go
+++ main.go
@@ -5,13 +5,28 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/googleinterns/terraform-cost-estimation/billing"
+	"github.com/googleinterns/terraform-cost-estimation/io"
 	"github.com/googleinterns/terraform-cost-estimation/jsdecode"
 )
 
-var out = flag.String("out", "stdout", `Write the cost estimation to a given file path. If set to 'stdout',
-the output will be shown in the command line.`)
+var (
+	output = flag.String("output", "stdout", `Write the cost estimations to the given paths.
+If set to 'stdout', all the outputs will be shown in the command line.
+Multiple output file names must be delimited by ','.
+Mixed file names and stdout values are allowed.`)
+	format = flag.String("format", "txt", `Write the pricing information in the specified format.
+Can be set to: txt, json, html.`)
+)
+
+func minInt(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
 
 func main() {
 	flag.Usage = func() {
@@ -29,58 +44,52 @@ func main() {
 		os.Exit(1)
 	}
 
-	if len(flag.Args()) > 1 {
-		fmt.Fprintf(os.Stderr, "Error: Too many argumets\n\n")
-		flag.Usage()
-		os.Exit(1)
-	}
-
-	fin, errFin := os.Open(flag.Args()[0])
-	if errFin != nil {
-		fmt.Fprintf(os.Stderr, "Error: "+errFin.Error()+"\n")
-		os.Exit(1)
-	}
-
-	plan, err := jsdecode.ExtractPlanStruct(fin)
-	if err != nil {
-		fmt.Fprint(os.Stderr, "Error:"+err.Error()+"\n")
-		os.Exit(2)
-	}
-
-	if errFin = fin.Close(); errFin != nil {
-		fmt.Fprintf(os.Stderr, "Error: "+errFin.Error()+"\n")
-		os.Exit(1)
-	}
-
-	resources := jsdecode.GetResources(plan)
-
-	computeEngineCatalog, catalogErr := billing.NewComputeEngineCatalog(context.Background())
-	if catalogErr != nil {
-		fmt.Fprintf(os.Stderr, "Error: "+catalogErr.Error())
-		os.Exit(4)
-	}
-
-	var fout *os.File
-	var errFout error
-	if *out == "stdout" {
-		fout = os.Stdout
-	} else {
-		fout, errFout = os.Open(*out)
-		if errFout != nil {
-			fmt.Fprintf(os.Stderr, "Error: "+errFout.Error()+"\n")
-			os.Exit(3)
+	outputFileNames := strings.Split(*output, ",")
+	if *output != "stdout" {
+		if len(outputFileNames) != len(flag.Args()) {
+			fmt.Fprintf(os.Stderr, "Error: Input and output files number differ.\n\n")
+			flag.Usage()
+			os.Exit(1)
 		}
 	}
 
-	for _, r := range resources {
-		r.CompletePricingInfo(computeEngineCatalog)
-		r.PrintPricingInfo(fout)
+	computeEngineCatalog, catalogErr := billing.NewComputeEngineCatalog(context.Background())
+	if catalogErr != nil {
+		fmt.Fprintf(os.Stderr, "Error: "+catalogErr.Error()+"\n\n")
+		os.Exit(2)
 	}
 
-	if errFout = fout.Close(); errFout != nil {
-		fmt.Fprintf(os.Stderr, "Error: "+errFout.Error()+"\n")
-		os.Exit(1)
-	}
+	for i := range flag.Args() {
+		plan, err := io.GetPlan(flag.Arg(i))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: "+err.Error()+"\n\n")
+			os.Exit(3)
+		}
 
-	// TODO: print output
+		resources := jsdecode.GetResources(plan)
+
+		fout, err := io.GetOutputWriter(outputFileNames[minInt(i, len(outputFileNames)-1)])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: "+err.Error()+"\n\n")
+		}
+
+		fout.Write([]byte(fmt.Sprintf("Pricing information for %s:\n\n", flag.Arg(i))))
+		summary := ""
+
+		for _, r := range resources {
+			if err = r.CompletePricingInfo(computeEngineCatalog); err != nil {
+				fmt.Fprintf(os.Stderr, "Error: "+err.Error()+"\n\n")
+				continue
+			}
+			r.PrintPricingInfo(fout)
+			summary += r.GetSummary() + "\n"
+		}
+
+		fout.Write([]byte("\n\nSummary:\n\n" + summary))
+
+		if err = io.FinishOutput(fout); err != nil {
+			fmt.Fprintf(os.Stderr, "Error: "+err.Error()+"\n\n")
+			os.Exit(3)
+		}
+	}
 }
